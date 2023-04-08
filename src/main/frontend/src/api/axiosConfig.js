5a81b8a57738bdbfb7c0f897af11a95b9e3d0ce4
Chinganshih <anna0935537079@gmail.com>
2023-04-07 21:56:34 -0400

    update baseURL to custom domain

--- src/main/frontend/src/api/axiosConfig.js
+++ src/main/frontend/src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://ec2-3-21-247-234.us-east-2.compute.amazonaws.com:8443',
+    baseURL:'https://moviereview.chingan.info:8443',
     //baseURL:'https://localhost:8443',
-    //headers: {"ngrok-skip-browser-warning": "true"}
 })
