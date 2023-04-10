794317c373ec6a886e29f5e1626ef35838902848
Chinganshih <anna0935537079@gmail.com>
2023-04-10 14:40:17 -0400

    add import css

--- src/main/frontend/src/components/trailer/Trailer.js
+++ src/main/frontend/src/components/trailer/Trailer.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
-
+import './Trailer.css'
 import React from 'react'
 
 const Trailer = () => {
@@ -12,8 +12,8 @@ const Trailer = () => {
 
   return (
     <div className="react-player-container">
-        {(key!=null)? <ReactPlayer controls='true' playing={true} url={`https://www.youtube.com/watch?v=${key}`}
-        width = '100%' height = '100%'/> : null}
+        {(key!=null)?<ReactPlayer controls='true' playing={true} url={`https://www.youtube.com/watch?v=${key}`}
+        width = '100%' height = '100%'/>:null}
     </div>
   )
 }
